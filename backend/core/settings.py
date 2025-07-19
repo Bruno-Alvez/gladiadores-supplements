fe67f366d16bc59674b75734eca8d59cd310bb91
Bruno-Alvez <bruno.bsantos75@gmail.com>
2025-07-18 22:50:11 -0300

    fix(cors): remove duplicated CommonMiddleware to fix CORS headers

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -63,8 +63,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.common.CommonMiddleware',
 ]
+
 
 ROOT_URLCONF = 'core.urls'
 
